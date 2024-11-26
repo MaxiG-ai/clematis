58b5d1cc4d0e57bec9d2b39a6706b728997b74b0
Maxi <maxgraf27@gmail.com>
2024-11-26 11:19:15 +0100

    added example simulation

---
+++ src/manufacturing_example.py
@@ -0,0 +1,12 @@
+from clematis.model_generator_ns import ModelGeneratorNS
+from clematis.dynamic_manufacturing import DynamicManufacturing
+from igraph import *
+
+production_model = ModelGeneratorNS(n=14, s=10)
+
+work_stations, production_edges, vertex_attr = production_model.generate_graph()
+print(work_stations)
+
+manufacturing_sim = DynamicManufacturing(Graph(production_edges, directed=True, vertex_attrs=vertex_attr), seed=42)
+
+total_production, zero_count, one_count, two_count, state_array = manufacturing_sim.iterate(output="output.csv", write2file=True)
